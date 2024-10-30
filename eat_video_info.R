e2016cdf9ac0a4caaa460991510629b545b18629
eunji.oh <oheunji97@gmail.com>
2024-10-30 11:53:35 +0900

    update eat_video_info.R

--- eat_video_info.R
+++ eat_video_info.R
@@ -1,59 +1,69 @@
+# Load required libraries
 library(Hmisc)
 library(corrplot)
 library(plotly)
 
-setwd("~/Library/CloudStorage/GoogleDrive-oheunji97@gmail.com/내 드라이브/Document/2KAIST/2024/conference/ISMIR")
+# Set working directory
+setwd("/Users/user/workspace/ismir2024-emotion-inference")
 
+# Load and prepare data
 eat <- read.csv("target_rate.csv")
+eat <- subset(eat, select = -X) # Remove unnecessary column
 
-eat <- subset(eat, select = -X)
-
+# Convert relevant columns to factors
 eat$id <- as.factor(eat$id)
 eat$emotion <- as.factor(eat$emotion)
 eat$type <- as.factor(eat$type)
 
-emotion <- c("joy", "sad", "anger")
+# Define variables for easier referencing
+emotions <- c("joy", "sad", "anger")
 types <- c("music", "social")
 id_t <- unique(eat$id)
 
-
-for (i in seq_len(nrow(eat))){
-  if (eat[i, c("emotion")] == "joy"){
-    eat[i, c("pn")] <- "positive"
-  }
-  else if (eat[i, c("emotion")] != "joy"){
-    eat[i, c("pn")] <- "negative"
-  }
-}
-
+# Add 'pn' column for positive/negative classification based on 'emotion'
+eat$pn <- ifelse(eat$emotion == "joy", "positive", "negative")
 eat$pn <- as.factor(eat$pn)
 
-summary(eat[eat$emotion=="sad",])
-sd(eat[eat$emotion=="sad","dominance"])
+# Quick summary and standard deviation calculations for 'sad' emotion
+summary(eat[eat$emotion == "sad", ])
+sd(eat[eat$emotion == "sad", "dominance"])
 
-df_eat <- eat[,c("id","emotion","valence","arousal","dominance")]
+# Select relevant columns for 3D plot
+df_eat <- eat[, c("id", "emotion", "valence", "arousal", "dominance")]
 
-
-
-# 3-d AVD model
-
-fig <- plot_ly(df_eat, x = ~valence, y = ~arousal, z = ~dominance,
-        mode="markers", 
-        symbol = ~emotion, symbols = c('square', 'cross', 'circle'), 
-        color = ~emotion, colors = c('#fb4f4f', '#fbc93d', '#6cc0e5')) %>%
+# 3D AVD (Arousal-Valence-Dominance) Model Visualization
+fig <- plot_ly(df_eat,
+  x = ~valence, y = ~arousal, z = ~dominance,
+  mode = "markers",
+  symbol = ~emotion, symbols = c("square", "cross", "circle"),
+  color = ~emotion, colors = c("#fb4f4f", "#fbc93d", "#6cc0e5")
+) %>%
   add_markers() %>%
-  layout(scene = list(xaxis = list(title = '<b> Valence </b>',
-                                   titlefont = list(size = 20),
-                                   tickfont = list(size = 15)),
-                      yaxis = list(title = '<b> Arousal </b>',
-                                   titlefont = list(size = 20),
-                                   tickfont = list(size = 15)),
-                      zaxis = list(title = '<b> Dominance </b>',
-                                   titlefont = list(size = 20),
-                                   tickfont = list(size = 15))),
-        legend = list(title=list(text='<b> Emotion </b>',
-                                 font = list(size = 16))))
+  layout(
+    scene = list(
+      xaxis = list(
+        title = "<b> Valence </b>",
+        titlefont = list(size = 20),
+        tickfont = list(size = 15)
+      ),
+      yaxis = list(
+        title = "<b> Arousal </b>",
+        titlefont = list(size = 20),
+        tickfont = list(size = 15)
+      ),
+      zaxis = list(
+        title = "<b> Dominance </b>",
+        titlefont = list(size = 20),
+        tickfont = list(size = 15)
+      )
+    ),
+    legend = list(
+      title = list(
+        text = "<b> Emotion </b>",
+        font = list(size = 16)
+      )
+    )
+  )
 
+# Display the 3D plot
 fig
-
-
